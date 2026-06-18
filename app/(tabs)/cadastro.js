1abca5a14e8186935a0bfee41c52a7264071b5eb
Rayan Lucas Lô Mendes <rayan58346916@edu.df.senac.br>
2026-06-18 15:16:31 -0300

    Fazendo melhorias ao server.js

--- app/(tabs)/cadastro.js
+++ app/(tabs)/cadastro.js
@@ -3,12 +3,120 @@ import {
     Text, // Para exibir textos (= p, h1...)
     TouchableOpacity, // Para botões clicáveis (= button)
     ScrollView, // Para a área principal com scroll,
-    StyleSheet // Para aplicar estilo na página
+    StyleSheet, // Para aplicar estilo na página
+    TextInput
    } from 'react-native'; // Importa os componentes View e Text
    import {Link} from 'expo-router';
-import { TextInput } from 'react-native-web';
+   import {useState} from 'react';
+
+   const API_URL = "http://localhost:3000"
    
   export default function Cadastro() {
+    const[nome,setNome] = useState('');
+    const[email,setEmail] = useState('');
+    const[senha,setSenha] = useState('');
+    const[confirmarSenha,setConfirmarSenha] = useState('');
+
+    const[mensagemSistema,setMensagemSistema] = useState('');
+    const[tipoMensagem,setTipoMensagem] = useState('');
+
+   async function validarCadastro(){
+    if(nome === ''){
+      setMensagemSistema("Digite seu nome!");
+      setTipoMensagem("erro");
+      return
+    }
+
+    if(/\d/.test(nome)){
+      setMensagemSistema("O nome não pode conter número!");
+      setTipoMensagem("erro");
+      return
+    }
+
+    if(email === ''){
+      setMensagemSistema("Digite seu email!");
+      setTipoMensagem("erro");
+      return
+    }
+    if(!email.includes("@") || !email.includes(".com")){
+      setMensagemSistema("Digite um e-mail válido!");
+      setTipoMensagem("erro");
+      return
+    }
+
+    if(senha === ''){
+      setMensagemSistema("Digite sua senha!");
+      setTipoMensagem("erro");
+      return
+    }
+
+    if(senha.length < 6 ){
+      setMensagemSistema("A senha deve ter pelo menos 6 caracteres!");
+      setTipoMensagem("erro");
+      return
+    }
+
+    if(confirmarSenha === ''){
+      setMensagemSistema("Digite novamente sua senha!");
+      setTipoMensagem("erro");
+      return
+    }
+    
+    if(confirmarSenha.length < 6 ){
+      setMensagemSistema("A senha deve ter pelo menos 6 caracteres!");
+      setTipoMensagem("erro");
+      return
+    }
+
+    if(senha !== confirmarSenha){
+      setMensagemSistema("As senhas devem ser iguais.");
+      setTipoMensagem("erro");
+      return
+    }
+    //Tenta executar o bloco, se houver erro de rede, o código vai para o cath
+    try{
+      // Faz uma requisição HTTP para a rota da API usando o método POST
+      const resposta = await fetch(`${API_URL}/cadastro`,{
+        method: 'POST', // Define que a requisição vai ENVIAR DADOS
+        headers: {'Content-Type': 'application/json'}, // Informa que o corpo da requisição está JSON
+        credentials:'include', // Inclui cookies e sessão na requisição, útil para autenticação
+        body: JSON.stringify({
+          nome: nome,
+          email: email,
+          senha: senha
+        }) // Converte os dados de JavaScript para texto JSON antes de enviar
+      });
+      // Converte a resposta recebida da API de JSON para objeto JavaScript
+      const dados = await resposta.json() 
+
+      // Verifica se a resposta HTTP foi de sucesso
+      if(resposta.ok){
+        // Mostra a mensagem de sucesso vinda da API, 
+          //ou um texto padrão se ela não enviar nada
+        setMensagemSistema(dados.mensagem || "Cadastro realizado")
+        // Define o "estilo" da mensagem como sucesso
+        setTipoMensagem("sucesso")
+        // Limpa os campos do formulário
+        setNome('')
+        setEmail('')
+        setSenha('')
+        setConfirmarSenha('')
+      } else{
+        // Mostra a mensagem de erro vinda da API,
+          // ou um texto padrão se ela não enviar nada
+        setMensagemSistema(dados.erro || "Erro ao cadastrar")
+        // Define o "estilo" da mensagem como erro
+        setTipoMensagem("erro") 
+      }
+    }catch(erro){
+      //  Executado quando acontece falha na conexão,
+        // como internet fora do ar ou servidor indisponivel
+      setMensagemSistema("Erro ao conectar com o servidor")
+      // Define o "estilo" da mensagem como erro
+      setTipoMensagem("erro")
+    }
+  }
+
    return (
       <ScrollView>
           { /*=========== TOPO (HEADER) =============*/}
@@ -36,41 +144,70 @@ import { TextInput } from 'react-native-web';
           </View>
 
           { /*=========== CONTEÚDO DA PÁGINA =============*/}
-         <View style={styles.container}> 
+          <View style={styles.container}>
             <View style={styles.paginaAuth}>
-                    <Text style={styles.tituloAuth}>
-                    Cadastro
-                    </Text>
-                    <Text style={styles.textAuth}>
-                        Crie sua conta para acessar os cursos
-                    </Text>
+                <Text style={styles.tituloAuth}>
+                  Cadastro
+                </Text>
+                <Text style={styles.textoAuth}>
+                  Crie sua conta para acessar os cursos
+                </Text>
 
                 <View style={styles.blocoAuth}>
-                    <Text style={styles.label}>Nome</Text>
-                    <TextInput placeholder='Digite seu nome'style={styles.input}></TextInput>
+                  <Text style={styles.label}>Nome</Text>
+                  <TextInput placeholder='Digite seu nome'
+                            value={nome} 
+                            onChangeText={setNome}
+                            style={styles.input}>
+                  </TextInput>
+                
+                  <Text style={styles.label}>Email</Text>
+                  <TextInput placeholder='Digite seu email' 
+                             keyboardType='email-address'
+                             value={email} 
+                             onChangeText={setEmail}
+                             style={styles.input}>
+                  </TextInput>
 
-                    <Text style={styles.label}>Email</Text>
-                    <TextInput placeholder='Digite seu e-mail' keyboardType='email-address' style={styles.input}></TextInput>
+                  <Text style={styles.label}>Senha</Text>
+                  <TextInput placeholder='Digite sua senha' 
+                             secureTextEntry={true}
+                             value={senha} 
+                             onChangeText={setSenha}
+                             style={styles.input}>
+                  </TextInput>
 
-                    <Text style={styles.label}>Senha</Text>
-                    <TextInput placeholder='Digite sua senha' secureTextEntry={true} style={styles.input}></TextInput>
+                  <Text style={styles.label}>Confirma senha</Text>
+                  <TextInput placeholder='Digite sua senha novamente' 
+                             secureTextEntry={true}
+                             value={confirmarSenha} 
+                             onChangeText={setConfirmarSenha}
+                             style={styles.input}>
+                  </TextInput>
 
-                    <Text style={styles.label}>Confirmar senha</Text>
-                    <TextInput placeholder='Digite sua senha novamente' secureTextEntry={true} style={styles.input}></TextInput>
+                  <TouchableOpacity style = {styles.btnPrimario} 
+                                    onPress={validarCadastro}>
+                    <Text style= {styles.textoBotao}>
+                      Cadastrar
+                    </Text>
+                  </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.btnPrimario}>
-                        <Text style={styles.textoBotao}> Cadastrar </Text>
-                    </TouchableOpacity>
+                  <Text style={tipoMensagem === "erro" ? 
+                              styles.mensagemErro : styles.mensagemSucesso}>{mensagemSistema}</Text>
 
-                    <Text style={styles.mensagemAuth}></Text>
+                  <Text style={styles.linkAuth}>
+                    Já possui uma conta?
+                  </Text>
+                  <Link style={styles.linkAuthDestaque} href='/login'>
+                    <Text>
+                      Fazer Login
+                    </Text>
+                  </Link>
+                
 
-                    <Text style={styles.linkAuth}> Já possui uma conta? </Text>
-                    <Link style={styles.LinkAuthDestaque} href='/login'>
-                        <Text > Fazer Login</Text>
-                    </Link>
-                </View>
+              </View>
             </View>
-         </View>
+          </View>
 
           { /*=========== RODAPÉ =============*/}
           { /* Parte final da página */}
@@ -128,7 +265,6 @@ import { TextInput } from 'react-native-web';
         padding: 20,
       },
 
-
       paginaAuth: {
         paddingVertical: 40,
         paddingHorizontal: 20,
@@ -136,6 +272,7 @@ import { TextInput } from 'react-native-web';
 
       blocoAuth: {
         backgroundColor: '#fffff',
+        gap: 10,
         padding: 30,
         borderRadius: 8,
         elevation: 3,
@@ -159,7 +296,8 @@ import { TextInput } from 'react-native-web';
         linkAuth: {
             textAlign:'center',
             marginTop: 20,
-            color: '#222'
+            color: '#222',
+            fontWeight: 'bold'
         },
 
         linkAuthDestaque: {
@@ -167,7 +305,6 @@ import { TextInput } from 'react-native-web';
             fontWeight: 'bold',
             textAlign: 'center',
             marginTop: 5,
-            
         },
 
         tituloAuth: {
@@ -200,26 +337,33 @@ import { TextInput } from 'react-native-web';
         },
 
         btnPrimario: {
-            backgroundColor: '#ff6a00',
-            paddingVertical: 12,
-            paddingHorizontal: 20,
-            borderRadius: 8,
-            marginTop: 10,
+          backgroundColor: '#ff6a00',
+          paddingVertical: 12,
+          paddingHorizontal: 20,
+          borderRadius: 8,
+          marginTop: 10
         },
 
         textoBotao: {
-            color: '#ffffff',
-            fontWeight: 'bold',
-
+          color: '#ffffff',
+          fontWeight: 'bold',
+          textAlign: 'center'
         },
 
-        mensagemAuth: {
-            textAlign: 'center',
-            fontWeight: 'bold',
-            marginTop: 10,
-            marginHeight: 20,
-        },
-  
+        mensagemSucesso: {
+          textAlign: 'center',
+          fontWeight: 'bold',
+          color:"green",
+          marginTop: 15
+      },
+
+      mensagemErro: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color:"red",
+        marginTop: 15
+      },
+      
       rodape: {
         backgroundColor: '#1a4db3',
         padding: 20,
